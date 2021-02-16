4bcb755efe7ab16a22a2f31c16b94aff5a80d78f
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 14:42:01 -0300

    Add files via upload

---
+++ Uri1075/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Uri1075
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, num;
+            n = int.Parse(Console.ReadLine());
+
+            while (n > 10000)
+            {
+                Console.WriteLine("digite novamente");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            for (int c = 1; c <= 10000; c++)
+            {
+                if(c%n==2)
+                {
+                    Console.WriteLine(c);
+                }
+
+            }
+
+
+        }
+    }
+}
